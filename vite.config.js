91f6fbfb656963db35147dee51be1e94d9865795
saagarnkashyap <saagarcourses@gmail.com>
2025-05-30 03:23:52 +0530

    Fix: correct main.jsx path and base config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/saagarkashyap/',  // <-- 🔥 add this line: your repo name with slashes
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ['react-tsparticles', 'tsparticles']
