ef63cf05546f2d6eebad89b038573bbdd99fd118
Saagar N Kashyap <saagarcourses@gmail.com>
2025-12-02 22:58:40 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/saagarkashyap/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 2000,
   },
